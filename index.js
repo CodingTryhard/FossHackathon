2d62ea6114d22b7c29a5c5d9820be2829a87f231
A0D1I2L3 <adilhaneefa2005@gmail.com>
2024-04-06 01:11:48 +0530

    dashboard

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import { dirname } from "path";
+import bodyParser from "body-parser";
+
 import { fileURLToPath } from "url";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
@@ -7,25 +9,27 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/login", (req, res) => {
-  res.sendFile(__dirname + "/public/login.html");
+  res.sendFile(__dirname + "/public/htmls/login.html");
 });
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/home.html");
+  res.sendFile(__dirname + "/public/htmls/home.html");
 });
 
 app.get("/signup", (req, res) => {
-  res.sendFile(__dirname + "/public/signup.html");
+  res.sendFile(__dirname + "/public/htmls/signup.html");
 });
 app.get("/login", (req, res) => {
-  res.sendFile(__dirname + "/public/login.html");
+  res.sendFile(__dirname + "/public/htmls/login.html");
 });
 app.post("/dashboard", (req, res) => {
-  res.sendFile(__dirname + "/public/dashboard.html");
+  res.render("dashboard.ejs", {
+    username: req.body["Username"],
+  });
 });
-
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
