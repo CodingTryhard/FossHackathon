081ef4de26a27793c9f0ae9ee5e775b027febd1e
A0D1I2L3 <adilhaneefa2005@gmail.com>
2024-04-06 09:49:07 +0530

    main commit

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { dirname } from "path";
 import bodyParser from "body-parser";
 
 import { fileURLToPath } from "url";
+import { error } from "console";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -26,11 +27,30 @@ app.get("/login", (req, res) => {
   res.sendFile(__dirname + "/public/htmls/login.html");
 });
 app.post("/dashboard", (req, res) => {
-  res.render("dashboard.ejs", {
-    username: req.body["Username"],
-  });
+  res.sendFile(__dirname + "/public/htmls/dashboard.html");
 });
 
+app.get("/calender", (req, res) => {
+  res.sendFile(__dirname + "/public/htmls/calender.html");
+});
+app.get("/youtube", (req, res) => {
+  res.sendFile(__dirname + "/public/htmls/youtube.html");
+});
+app.post("/youtube_page", (req, res) => {
+  let originalString = req.body["link"];
+
+  let newString = originalString.replace("watch?v=", "embed/");
+
+  try {
+    res.render("youtube_page.ejs", {
+      newString: newString,
+    });
+  } catch (e) {
+    res.send(
+      "<h1>Invalid Youtube link ,make sure you paste links that looks like youtube.com/watch?v=VideoId<h1>"
+    );
+  }
+});
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
